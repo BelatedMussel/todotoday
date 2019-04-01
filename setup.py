a4a69d8945fcc8fef09740c9330911beecd4390b
BelatedMussel <j.d.kurtenbach@gmail.com>
2019-04-01 12:17:55 -0600

    Fixed PyPi classifier.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
-        "License :: OSI Approved :: GNU GPLv3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
